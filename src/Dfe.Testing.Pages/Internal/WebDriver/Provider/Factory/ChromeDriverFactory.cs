663b64db84c1d0fccc5dbf29b317d726cab2c844
Aasim AHMED <Aasim.AHMED@EDUCATION.GOV.UK>
2025-01-14 14:06:01 +0000

    remove commented code

--- src/Dfe.Testing.Pages/Internal/WebDriver/Provider/Factory/ChromeDriverFactory.cs
+++ src/Dfe.Testing.Pages/Internal/WebDriver/Provider/Factory/ChromeDriverFactory.cs
@@ -1,19 +1,6 @@
 ﻿using OpenQA.Selenium.Chrome;
 
 namespace Dfe.Testing.Pages.Internal.WebDriver.Provider.Factory;
-//internal sealed class ChromeDriverFactory : WebDriverFactoryBase<ChromeDriver>
-//{
-//    public override Task<Func<ChromeDriver>> CreateDriver(WebDriverSessionOptions sessionOptions)
-//    {
-//        // TODO could enable caching driverService in the abstract class - so that when new ChromeDriver it's created from the cached instance?
-//        var driverService = ChromeDriverService.CreateDefaultService();
-//        ChromeOptions options = new();
-//        //options.AddArgument("--headless");
-//        return Task.FromResult(
-//            () => new ChromeDriver(driverService, options));
-//    }
-//}
-
 
 internal sealed class ChromeDriverFactory : IBrowserFactory
 {
